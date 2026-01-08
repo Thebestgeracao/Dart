5c2326a5e27b422cad31d5fe76b928d182ad8eec
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-08 19:19:58 -0300

    Escrevendo a ficha de participante 🎉
    Escrevendo a ficha de participante 🎉

---
+++ ficha/bin/ficha.dart
@@ -0,0 +1,13 @@
+
+void main() {
+  String nome = 'Natan Duck';
+  String cpf = "576.805.763-12";
+  bool membro = true;
+  double altura = 1.80;
+  int idade = 46;
+
+  print("Eu sou $nome");
+  print("Meu CPF: $cpf");
+  print("Sou membro da comunidade? $membro");
+  print("Eu tenho $altura metro de altura e $idade anos de idade.");
+}
