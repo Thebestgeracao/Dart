4359099102a5a293b1161120ca9fb242ef664653
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-13 18:41:22 -0300

    Implementando a verificação de saldo 🎉
    Implementando a verificação de saldo 🎉

---
+++ verificacao_saldo/bin/verificacao_saldo.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+double saldo = 1000.0; 
+
+void main() {
+  print('Boas-vindas ao seu banco digital!');
+  print('Seu saldo atual é de: R\$${saldo.toStringAsFixed(2)}');
+
+  print('Digite o valor do Pix que deseja realizar:');
+  double valorPix = double.parse(stdin.readLineSync()!);
+
+  realizarPix(valorPix);
+}
+
+void realizarPix(double valor) {
+  if (valor <= saldo) {
+    saldo -= valor;
+    print('Pix de R\$${valor.toStringAsFixed(2)} realizado com sucesso.');
+    print('Seu novo saldo é de: R\$${saldo.toStringAsFixed(2)}');
+  } else {
+    print('Saldo insuficiente para realizar o Pix.');
+  }
+}
---
+++ verificacao_saldo/test/verificacao_saldo_test.dart
@@ -0,0 +1,8 @@
+import 'package:verificacao_saldo/verificacao_saldo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
