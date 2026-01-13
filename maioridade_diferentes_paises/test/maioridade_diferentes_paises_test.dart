aa9a812219189428bf2b793d8653a7042e39816d
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-13 19:03:41 -0300

    Verificando a maioridade em diferentes países 🎉
    Verificando a maioridade em diferentes países 🎉

---
+++ maioridade_diferentes_paises/test/maioridade_diferentes_paises_test.dart
@@ -0,0 +1,8 @@
+import 'package:maioridade_diferentes_paises/maioridade_diferentes_paises.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
