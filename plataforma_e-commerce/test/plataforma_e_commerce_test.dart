08c3dc69923fe56daf70225756ff7d870546812b
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-16 22:01:26 -0300

    Validação de métodos de pagamento em uma plataforma de e-commerce 🎉

---
+++ plataforma_e-commerce/test/plataforma_e_commerce_test.dart
@@ -0,0 +1,8 @@
+import 'package:plataforma_e_commerce/plataforma_e_commerce.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
