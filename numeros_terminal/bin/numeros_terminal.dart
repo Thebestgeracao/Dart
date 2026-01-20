d21902d2ec6eba487362aa3b33f7c0b0338bc2ee
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-19 23:27:59 -0300

    Imprimindo números no terminal 🎉

---
+++ numeros_terminal/bin/numeros_terminal.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  int limite = 5;
+
+  for(int i = 1; i <= 5; i++){
+    print("O valor de i é: $i");
+  }
+}
+
+
+  
---
+++ numeros_terminal/test/numeros_terminal_test.dart
@@ -0,0 +1,8 @@
+import 'package:numeros_terminal/numeros_terminal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
