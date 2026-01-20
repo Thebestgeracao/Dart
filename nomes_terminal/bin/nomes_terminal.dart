fb2d1ec89f74f34563e0f3c8acd3fc9fdc9ee2b0
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-19 23:28:51 -0300

    Mostrando nomes no terminal 🎉

---
+++ nomes_terminal/bin/nomes_terminal.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> nomes = ["Ana", "João", "Maria"];
+
+  for (String nome in nomes) {
+    print("Nome: $nome");
+  }
+}
---
+++ nomes_terminal/test/nomes_terminal_test.dart
@@ -0,0 +1,8 @@
+import 'package:nomes_terminal/nomes_terminal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
