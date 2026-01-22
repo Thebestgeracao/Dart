c706e78aa5e0a0e40cf9ded5ca48850379f2ce3b
Matheus De Jesus <matheuscostajesus@hotmail.com>
2026-01-22 16:16:21 -0300

    Sistema de acompanhamento escolar 🎉

---
+++ registrar_nome_notas_escolares/test/registrar_nome_notas_escolares_test.dart
@@ -0,0 +1,8 @@
+import 'package:registrar_nome_notas_escolares/registrar_nome_notas_escolares.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
